3a96a75339f806e0460b7fbf8c58306b05034fb6
Robin Palotai <palotai.robin@gmail.com>
2016-06-22 07:46:23 +0200

    Tmux, xterm colors, source code font.

--- rigo.pp
+++ rigo.pp
@@ -60,7 +60,26 @@ node default {
   package { "zip": ensure => installed }
   package { "unrar": ensure => installed }
 
-  package { "tmux": ensure => installed }
+  package {
+    "tmux": ensure => installed
+  } ->
+  file { '/etc/tmux.conf':
+    ensure => present,
+    mode => '0644',
+  } ->
+  file_line { 'tmux-vikeys':
+    path => '/etc/tmux.conf',
+    line => 'setw -g mode-keys vi',
+  } ->
+  file_line { 'tmux-fastescape':
+    path => '/etc/tmux.conf',
+    line => 'set -s escape-time 0',
+  } ->
+  file_line { 'tmux-colors':
+    path => '/etc/tmux.conf',
+    line => 'set -g default-terminal "screen-256color"',
+  }
+
   package { "mc": ensure => installed }
   package { "links": ensure => installed }
   package { "htop": ensure => installed }
@@ -98,6 +117,8 @@ node default {
   package { "vim-bufexplorer": ensure => installed }
   package { "vim-tagbar": ensure => installed }
 
+  package { "adobe-source-code-pro-fonts": ensure => installed }
+
   package { "emacs": ensure => installed }
 
   # Add dep on user and git?
@@ -107,6 +128,20 @@ node default {
     source   => 'https://github.com/syl20bnr/spacemacs',
     revision => 'v0.105.18',
     user     => 'ron',
+  }
+
+  file { '/home/ron/.Xresources':
+    ensure => present,
+    mode => '0644',
+    owner => 'ron',
+  } ->
+  file_line { 'ron_xresources_xterm_colors':
+    path => '/home/ron/.Xresources',
+    line => 'XTerm*termName: xterm-256color',
+  } ->
+  file_line { 'ron_xresources_xterm_inverted':
+    path => '/home/ron/.Xresources',
+    line => 'XTerm*reverseVideo: on',
   }
 
   exec { 'retrieve_bazel_installer':
